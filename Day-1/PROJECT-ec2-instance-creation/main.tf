c66a30526b5f29170c93a1f237bf6069ef0e96cb
Gopichand Kosuri <97935918+GopichandKosuri739@users.noreply.github.com>
2024-11-26 12:06:44 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,14 @@
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
+    access_key = "AKIA4WF4MS75YP66ZMGU"
+    secret_key = "jBjx4ucn6rc/PYCF6MwBGgqAIyx6THD9cfAF1HTX"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "local-example" {
+    tags = {
+            Name = "terraform-EC2-local"  # Add a name tag here
+        }
+
+    ami           = "ami-0166fe664262f664c"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
